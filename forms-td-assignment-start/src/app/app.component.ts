10ad35e4b47875ec6d5584eb28b96bf0261b2b15
Rafał Zubala <rzubala@gmail.com>
2019-02-12 08:26:58 +0100

    forms-td-assignment-start

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,10 +6,23 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   @ViewChild('f') form: NgForm;
+  submitted: boolean = false;
+  data = {mail: '', subscription: '', password: ''};
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+
+  ngOnInit() {
+
+  }
 
   onSubmit() {
     console.log(this.form);
+
+    this.data.mail = this.form.value.email;
+    this.data.subscription = this.form.value.subscription;
+    this.data.password = this.form.value.password;
+    this.submitted = true;
+    this.form.form.reset();
   }
 }
