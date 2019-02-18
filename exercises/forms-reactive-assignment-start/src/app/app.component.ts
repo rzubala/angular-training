39992fdfe2157603a9cd893d32923548702bdad7
Rafał Zubala <rzubala@gmail.com>
2019-02-18 21:20:28 +0100

    solved

--- exercises/forms-reactive-assignment-start/src/app/app.component.ts
+++ exercises/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  projectForm: FormGroup;
+  statuses: string[] = ['Stable', 'Critical', 'Finished']
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'name': new FormControl(null, Validators.required, this.validateProjectNameAsync),
+      'mail': new FormControl(null, [ Validators.required, Validators.email]),
+      'status': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
+  validateProjectName(input: FormControl) : {[s: string]: boolean} {
+    if (input.value === 'Test') {
+      return {'forbiddenName': true};
+    }
+    return null;
+  }
+
+  validateProjectNameAsync(input: FormControl) : Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (input.value === 'Test') {
+          resolve({'forbiddenName': true});
+        } else {
+          resolve(null);
+        }
+        
+      }, 1500);
+    });
+    return promise;
+  }
 }
