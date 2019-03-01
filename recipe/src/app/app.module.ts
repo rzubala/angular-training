e2fc66fb878485b8d572503b317d472e26e39c94
Rafał Zubala <rzubala@gmail.com>
2019-03-01 20:09:18 +0100

    switch to httpClient

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HttpModule } from '@angular/http';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -12,7 +12,7 @@ import { AuthModule } from './auth/auth.module';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule, 
     AuthModule,    
     CoreModule
   ],
--- recipe/src/app/shared/data-storage.service.ts
+++ recipe/src/app/shared/data-storage.service.ts
@@ -1,22 +1,22 @@
-import { Http, Response } from "@angular/http";
 import { map, catchError } from 'rxjs/operators';
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { throwError } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { AuthService } from "../auth/auth.service";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class DataStorageService {
 
-    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     getRecipes() {
         const token = this.authService.getToken();
 
-        this.http.get('https://ng-recipe-book-89723.firebaseio.com/recipes.json?auth=' + token)
-        .pipe(map((response: Response) => {
-            const recipes: Recipe[] = response.json();
+        this.httpClient.get<Recipe[]>('https://ng-recipe-book-89723.firebaseio.com/recipes.json?auth=' + token)
+        .pipe(map((recipes) => {
+            //const recipes: Recipe[] = response.json();    //http
             for (let recipe of recipes) {
                 if (!recipe['ingredients']) {
                     console.log(recipe)
@@ -35,7 +35,7 @@ export class DataStorageService {
     storeRecipies() {
         const token = this.authService.getToken();
 
-        return this.http.put('https://ng-recipe-book-89723.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes())
+        return this.httpClient.put('https://ng-recipe-book-89723.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes())
         .pipe(catchError((error: Response) => {
             return throwError('Something went wrong on save');
         }))
