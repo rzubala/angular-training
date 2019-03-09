f664404c589328ae1194eb34dbaf8e3269434aad
Rafał Zubala <rzubala@gmail.com>
2019-03-09 10:46:11 +0100

    shopping list store

--- ngrx/src/app/shopping-list/shopping-list.component.ts
+++ ngrx/src/app/shopping-list/shopping-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+
+import * as fromShoppingList from './store/shopping-list.reducers';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,14 +15,13 @@ import { Observable } from 'rxjs';
 export class ShoppingListComponent implements OnInit {
   shoppingListState: Observable<{ingredients: Ingredient[]}>;
 
-  constructor(private slService: ShoppingListService, private store: Store<{shoppingList: 
-    {ingredients: Ingredient[]}}>) {}
+  constructor(private store: Store<fromShoppingList.AppState>) {}
 
   ngOnInit() {
     this.shoppingListState = this.store.select('shoppingList');
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 }
--- ngrx/src/app/shopping-list/store/shopping-list.reducers.ts
+++ ngrx/src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,11 +1,23 @@
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 
-const initialState = {
+export interface AppState {
+    shoppingList: State;
+}
+
+export interface State {
+    ingredients: Ingredient[];
+    editedIngredient: Ingredient;
+    editedIngredientIndex: number;
+}
+
+const initialState: State = {
     ingredients: [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
-    ]
+    ],
+    editedIngredient: null,
+    editedIngredientIndex: -1
 };
 
 export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
@@ -17,6 +29,56 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
                   ...state.ingredients,
                   action.payload
               ]
+          }
+        case ShoppingListActions.ADD_INGREDIENTS:
+          return {
+              ...state,
+              ingredients: [
+                  ...state.ingredients,
+                  ...action.payload
+              ]
+          }
+        case ShoppingListActions.DELETE_INGREDIENT:
+          const indexD: number = state.editedIngredientIndex;
+          const ingredientsD = [...state.ingredients];
+          ingredientsD.splice(indexD, 1);
+          return {
+              ...state,
+              ingredients: [
+                  ...ingredientsD
+              ],
+              editedIngredient: null,
+              editedIngredientIndex: -1              
+          }
+        case ShoppingListActions.UPDATE_INGREDIENT:
+          const indexU: number = state.editedIngredientIndex;
+          const ingredient = state.ingredients[indexU];
+          const updatedIngredient = {
+              ...ingredient,
+              ...action.payload
+          }
+          const ingredients = [...state.ingredients]
+          ingredients[indexU] = updatedIngredient;
+          return {
+              ...state,
+              ingredients: [
+                  ...ingredients
+              ],
+              editedIngredient: null,
+              editedIngredientIndex: -1              
+          }
+        case ShoppingListActions.START_EDIT:
+          const editedIngredient = {...state.ingredients[action.payload]};
+          return {
+              ...state,
+              editedIngredient: editedIngredient,
+              editedIngredientIndex: action.payload
+          }
+        case ShoppingListActions.STOP_EDIT:
+          return {
+              ...state,
+              editedIngredient: null,
+              editedIngredientIndex: -1     
           }
           default:
             return state;
