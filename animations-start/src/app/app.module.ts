d7cfef6cbd52066cbfe803438ce00a4845103658
Rafał Zubala <rzubala@gmail.com>
2019-03-16 09:38:12 +0100

    angular animations

--- animations-start/src/app/app.module.ts
+++ animations-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule
